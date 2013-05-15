254898f9ffba094290c1fc7c2d3b9e82a1f1c368
Johannes Troeger <johannes.troeger@gmail.com>
2013-05-15 16:41:52 +0200

    0.2.0

--- angular-registry.js
+++ angular-registry.js
@@ -6,59 +6,59 @@
  * @author      Johannes Troeger <johannes.troeger@gmail.com>
  * @repository  https://github.com/johannestroeger/angular-registry
  * @license     http://www.wtfpl.net/ WTFPL – Do What the Fuck You Want to Public License
- * @version     0.1.1
+ * @version     0.2.0
  */
 
-angular.module('johannestroeger.registry', [])
-
-.provider('$registry', function () {
-
+angular.module('johannestroeger.registry', []).provider('$registry', function () {
   var register = {};
   var defaults = {};
-
+  var cacheLimit = 100;
   this.defaults = function (obj) {
     angular.extend(defaults, obj);
     angular.extend(register, defaults);
   };
-
-  this.$get = ['$parse', function ($parse) {
-
-    var cache = {};
-
-    var fnCache = function (exp) {
-      return (cache[exp]) ? cache[exp] : cache[exp] = $parse(exp);
-    };
-
-    var registry = function (root, exp, value, del) {
-      var parse = fnCache(exp);
-
-      if(value || del) {
-        return parse.assign(root, value);
-      }
-      if(exp) {
-        return parse(root);
-      }
-
-      return root;
-    };
-
-    return {
-      set: function (exp, value) {
-        return registry(register, exp, value);
-      },
-      get: function (exp) {
-        return registry(register, exp);
-      },
-      del: function (exp) {
-        return registry(register, exp, undefined, true);
-      },
-      reset: function(exp) {
-        if(exp) {
-          return registry(register, exp, registry(defaults, exp));
+  this.cacheLimit = function (limit) {
+    cacheLimit = limit;
+  };
+  this.$get = [
+    '$parse',
+    '$cacheFactory',
+    function ($parse, $cacheFactory) {
+      var cache = $cacheFactory('johannestroeger.registry', { capacity: cacheLimit });
+      var fnCache = function (exp) {
+        if (!cache.get(exp)) {
+          cache.put(exp, $parse(exp));
         }
-        register = {};
-        return angular.extend(register, defaults);
-      }
-    };
-  }];
+        return cache.get(exp);
+      };
+      var registry = function (root, exp, value, del) {
+        if (!exp) {
+          return root;
+        }
+        var getter = fnCache(exp);
+        if (value || del) {
+          return getter.assign(root, value);
+        }
+        return getter(root);
+      };
+      return {
+        set: function (exp, value) {
+          return registry(register, exp, value);
+        },
+        get: function (exp) {
+          return registry(register, exp);
+        },
+        del: function (exp) {
+          return registry(register, exp, undefined, true);
+        },
+        reset: function (exp) {
+          if (exp) {
+            return registry(register, exp, registry(defaults, exp));
+          }
+          register = {};
+          return angular.extend(register, defaults);
+        }
+      };
+    }
+  ];
 });
